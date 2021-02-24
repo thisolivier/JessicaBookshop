3d665a90b8c428c84f875d5039dbaa5170349f15
Olivier Butler <olivier.butler@gmail.com>
2021-02-24 21:00:40 +0000

    updated the palpal urls

--- paypalNotificationHandler/paypal.py
+++ paypalNotificationHandler/paypal.py
@@ -8,8 +8,8 @@ from paypalNotificationHandler import flaskInstance
 
 flaskInstance.config['PAYPAL_LIVE'] = False
 
-PAYPAL_LIVE_URL = 'https://www.paypal.com/cgi-bin/webscr'
-PAYPAL_SANDBOX_URL = 'https://www.sandbox.paypal.com/cgi-bin/webscr'
+PAYPAL_LIVE_URL = 'https://ipnpb.paypal.com/cgi-bin/webscr'
+PAYPAL_SANDBOX_URL = 'https://ipnpb.sandbox.paypal.com/cgi-bin/webscr'
 
 PAYPAL_URL = PAYPAL_LIVE_URL if flaskInstance.config['PAYPAL_LIVE'] else PAYPAL_SANDBOX_URL
 
@@ -45,4 +45,5 @@ def handleIPN():
         handle_authentic_IPN(request)
         return "Joyous times human, IPN message validated by PayPal", 200
     else:
+        print("paypalNotificationHander:paypal:40 - got an invalid response from paypal")
         return "Paypal said your message wasn't from them, stranger thing...", 403
